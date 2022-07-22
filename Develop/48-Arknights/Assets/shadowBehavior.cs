c1801ff700d06a289f87b2b57356e1ebaa7db562
唐傑伟 <tangjw_zhuhj@126.com>
2022-07-22 22:04:10 +0800

    美术素材
    预制体+图片

---
+++ Develop/48-Arknights/Assets/shadowBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadowBehavior : MonoBehaviour
+{
+    public TMPro.TextMeshPro tx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tx.text = "123";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
